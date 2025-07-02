e67935ae4e124b900657d3e720b6cd050855a1d3
Tyler Gobka Bright <tgbright88@gmail.com>
2025-07-02 19:26:26 +0000

    Implement cart functionality in FoodItem and StoreContext for adding/removing items

--- client/src/components/food.item/FoodItem.jsx
+++ client/src/components/food.item/FoodItem.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react'
+
 import { assets } from '../../assets/assets'
+import { useStoreContext } from '../../hooks/useStoreContext';
 
 const FoodItem = ({id, name, image, price, description}) => {
-    const [itemCount, setItemCount] = useState(0);
+    const {cartItems, addToCart, removeFromCart} = useStoreContext();
   return (
     <div key={id} className='flex flex-col items-start shadow rounded-2xl'>
         <div className='overflow-hidden rounded-2xl relative'>
-            <div className='z-10 absolute bottom-0 w-full h-20 bg-gradient-to-t from-orange-400 from-5% % to-transparent'><span className='h-10 w-20 rounded-tr-2xl rounded-br-2xl flex items-center justify-center bg-white text-orange-500 text-2xl font-semibold'>${price}</span></div>
+            <div className='z-10 absolute bottom-0 w-full h-20 bg-gradient-to-t from-orange-400 to-transparent'><span className='h-10 w-20 rounded-tr-2xl rounded-br-2xl flex items-center justify-center bg-white text-black text-xl font-semibold'>${price.toFixed(2)}</span></div>
             <img src={image} alt={name} className='z-0 w-full h-full rounded-2xl object-cover hover:scale-110 duration-500 ' />
-            <div className='absolute top-0 right-3 flex items-center p-3'>
-                {
-                !itemCount ? <img src={assets.add_icon_white} alt="" onClick={() => setItemCount(prev=>prev+1)} className='w-10' /> 
+            <div className='absolute top-0 right-3 bg-white rounded-full flex items-center p-1 mt-3'>
+                {!cartItems[id] ? <img src={assets.add_icon_white} alt="" onClick={() => addToCart(id)} className='w-10' /> 
                 : 
                 <div className='flex items-center gap-2'>
-                    <img src={assets.remove_icon_red} alt="" onClick={() => setItemCount(prev => prev - 1)} className='w-9' />
-                    <span className='text-white'>{itemCount}</span>
-                    <img src={assets.add_icon_green} alt="" onClick={() => setItemCount(prev => prev + 1)} className='w-9' />
+                    <img src={assets.remove_icon_red} alt="" onClick={() => removeFromCart(id)} className='w-9' />
+                    <span className='text-gray-600'>{cartItems[id]}</span>
+                    <img src={assets.add_icon_green} alt="" onClick={() => addToCart(id)} className='w-9' />
                 </div>
                 }
             </div>
@@ -26,7 +26,7 @@ const FoodItem = ({id, name, image, price, description}) => {
                 <img src={assets.rating_starts} alt="rate" className='w-20' />
             </div>
             <p className='text-gray-600'>{description}</p>
-            <p className='text-xl font-semibold text-orange-500'>${price}</p>
+            <p className='text-xl font-semibold text-orange-500'>${price.toFixed(2)}</p>
         </div>
     </div>
   )
--- client/src/contexts/StoreContext.jsx
+++ client/src/contexts/StoreContext.jsx
@@ -1,12 +1,42 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = ({children}) => {
 
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({
+            ...prev,
+            [itemId]: prev[itemId] ? prev[itemId] + 1 : 1
+        }));
+    };
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => {
+            if (!prev[itemId]) return prev;
+            const updatedCart = { ...prev };
+            if (updatedCart[itemId] === 1) {
+                delete updatedCart[itemId];
+            } else {
+                updatedCart[itemId] -= 1;
+            }
+            return updatedCart;
+        });
+    };
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems])
+
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
 
     return(
